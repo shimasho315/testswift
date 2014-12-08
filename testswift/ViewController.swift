a946b3e3847bd3c10dbc16455c37c972c5f18b5f
shimashima <das_juckt.15@ezweb.ne.jp>
2014-12-08 15:46:42 +0900

    Initial Commit

---
+++ testswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testswift
+//
+//  Created by 川島 祥吾 on 2014/12/08.
+//  Copyright (c) 2014年 川島 祥吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
