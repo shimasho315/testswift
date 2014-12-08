a027fff386e61044f205870bf6b06d7ab03f8a12
shimashima <das_juckt.15@ezweb.ne.jp>
2014-12-09 00:28:39 +0900

    ボタンを付けました。

--- testswift/ViewController.swift
+++ testswift/ViewController.swift
@@ -24,6 +24,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func tapBtn(sender: AnyObject) {
+        println("タップしました")
+    }
 
 
 }
